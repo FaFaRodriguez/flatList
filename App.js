60806c587624105075e33c5b61cb7a4208ac36fc
FaFaRodriguez <102042511+FaFaRodriguez@users.noreply.github.com>
2022-10-19 19:41:34 -0300

    Version 4
    Agregada funcionalidad de eliminar usuario

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, FlatList } from 'react-native';
 import React, { useState } from "react";
+import ListUsuario from './src/Componentes/Usuario/ListUsuarios';
 import Inputs from './src/Componentes/Inputs/Inputs';
 import Listadousuarios from './src/Componentes/ListadoUsuarios/ListadoUsuarios';
 
-
-
 export default function App() {
   const [arrayUsuario,SetArrayUsuario] = useState([{Nombre: "Jorge", Edad: 23, id: 0}, {Nombre: "Maria", Edad: 19, id: 1}]);
+
+ 
   return (
     <>
-      <Inputs arrayUsuario={arrayUsuario} SetArrayUsuario={SetArrayUsuario}/>
-      <Listadousuarios arrayUsuario= {arrayUsuario} SetArrayUsuario= {SetArrayUsuario}/>
+        <Inputs arrayUsuario={arrayUsuario} SetArrayUsuario={SetArrayUsuario}/>
+        <Listadousuarios arrayUsuario= {arrayUsuario} SetArrayUsuario= {SetArrayUsuario}/>  
     </>
 
   );
--- src/Componentes/Usuario/ListUsuarios.js
+++ src/Componentes/Usuario/ListUsuarios.js
@@ -1,40 +1,44 @@
 import { setStatusBarBackgroundColor } from "expo-status-bar";
 import React from "react";
-import {View, Text, StyleSheet, Button} from 'react-native'
+import {View, Text, StyleSheet, Button,  Alert, Modal, Pressable, useState} from 'react-native'
 
-const ListUsuario  = ({item}) =>{
+const ListUsuario  = (params) =>{
 
-    
     function EliminarUsuario() {
+        params.SetArrayUsuario(params.arrayUsuario.filter(item => item.id !== params.item.id));
+    }
+    function CambiarUsuario() {
         
     }
 
+  return (
+  
+    <>
 
 
-    return(
-        <>
-            <View style= {styles.container}>
-                <View style={{flex:5}}>
-                    <Text style = {styles.datos}> {item.Nombre}</Text>
-                    <Text style = {styles.datos}> {item.Edad}</Text>
-                </View>
-
-                <View style={{flex:4, marginVertical: 3}}>
-                    <Button
-                        title="Eliminar usuario"
-                        color="red"
-                        onPress={() => EliminarUsuario()}
-                    />
-                    <View style= {{marginVertical: 4}}/>
-                    <Button
-                        title="Cambiar datos"
-                        color="grey"
-                        onPress={() => CambiarUsuario()}
-                    />
-                </View>
-                
+    
+        <View style= {styles.container}>
+            <View style={{flex:5}}>
+                <Text style = {styles.datos}> {params.item.Nombre}</Text>
+                <Text style = {styles.datos}> {params.item.Edad}</Text>
             </View>
-        </>
+
+            <View style={{flex:4, marginVertical: 3}}>
+                <Button
+                    title="Eliminar usuario"
+                    color="red"
+                    onPress={() => EliminarUsuario()}
+                />
+                <View style= {{marginVertical: 4}}/>
+                <Button
+                    title="Cambiar datos"
+                    color="grey"
+                    onPress={() => CambiarUsuario()}
+                />
+            </View>
+            
+        </View>
+    </>
         
 
 
@@ -52,7 +56,48 @@ const styles = StyleSheet.create({
     },
     datos:{
         padding: 10
-    }
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+      },
+      buttonOpen: {
+        backgroundColor: "#F194FF",
+      },
+      buttonClose: {
+        backgroundColor: "#2196F3",
+      },
+      textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+      }
 
 })
 
